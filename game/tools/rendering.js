048927efc29d4320603c2bc40486cb1cb173b4a5
UnsignedByte <edlam1889@gmail.com>
2018-11-24 21:59:44 -0800

    move to folder

---
+++ game/tools/rendering.js
@@ -0,0 +1,7 @@
+/**
+ * @Author: Edmund Lam <edl>
+ * @Date:   21:59:40, 24-Nov-2018
+ * @Filename: rendering.js
+ * @Last modified by:   edl
+ * @Last modified time: 21:59:40, 24-Nov-2018
+ */
--- rendertools.js
+++
@@ -1,7 +0,0 @@
-/**
- * @Author: Edmund Lam <edl>
- * @Date:   21:16:25, 24-Nov-2018
- * @Filename: rendertools.js
- * @Last modified by:   edl
- * @Last modified time: 21:16:25, 24-Nov-2018
- */
