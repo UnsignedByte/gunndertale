def0c8f718395875cc9d86719daa575cbfc74fd8
UnsignedByte <edlam1889@gmail.com>
2018-11-27 22:42:45 -0800

    create coll.js

--- game/tools/collisions.js
+++ game/tools/collisions.js
@@ -3,14 +3,18 @@
  * @Date:   10:02:55, 27-Nov-2018
  * @Filename: collisions.js
  * @Last modified by:   edl
- * @Last modified time: 10:17:25, 27-Nov-2018
+ * @Last modified time: 19:16:30, 27-Nov-2018
  */
 
  var Collision = (function(){
    var self = {};
 
+   function c_c_l = function(){
+     mc.pos[0]-MOV_SPEED, mc.pos[1]+mc.currAnim.height;
+   }
+
    self.check_collide = function(){
-     mc.pos-1
+
    }
 
    return self;
