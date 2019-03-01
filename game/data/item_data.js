a9170a3ee3b1fec9847198b188ff415e5f1ac857
UnsignedByte <edlam1889@gmail.com>
2019-02-28 18:52:38 -0800

    prepare item data

--- game/data/item_data.js
+++ game/data/item_data.js
@@ -3,24 +3,43 @@
  * @Date:   16:09:05, 10-Feb-2019
  * @Filename: item_data.js
  * @Last modified by:   edl
- * @Last modified time: 16:35:33, 14-Feb-2019
+ * @Last modified time: 18:48:30, 28-Feb-2019
  */
 
 var ITEM_DATA = {
   "Airpods":{
-    description:"Makes you look rich."
+    description:"Makes you look rich.",
+    action:{
+      commands:[],
+      message:"*Laughs in rich*"
+    }
   },
   "Macbook":{
-    description:"Overpriced computer by Apple."
+    description:"Overpriced computer by Apple.",
+    action:{
+      commands:[],
+      message:"You felt superior to Chromebook users."
+    }
   },
   "Boba Milk Tea":{
-    description:"A drink"
+    description:"A drink",
+    action:{
+      commands:[],
+      message:"The milk tea was good."
+    }
   },
   "Frying Pan":{
     description:"It's a pan for frying.",
     action:{
       commands:[],
       message:"You used the frying pan. It didn't do much."
+    }
+  },
+  "Sweater":{
+    description:"Keeps you warm and cozy",
+    action:{
+      commands:[],
+      message:"You wore the sweater and feel cool."
     }
   }
 }
