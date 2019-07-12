239d5d6d37ba04c0eac308157001338f70de7bb0
UnsignedByte <edlam1889@gmail.com>
2019-07-12 14:44:35 -0700

    bruh

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # GUNNDERTALE
 
 Inspired by [gunn student sim](https://sheeptester.github.io/gunn-student-sim/) by sheeptester
+
+Art by [Nichodon](https://github.com/nichodon)
--- game/tools/stathandler.js
+++ game/tools/stathandler.js
@@ -3,7 +3,7 @@
  * @Date:   23:01:22, 06-Mar-2019
  * @Filename: stathandler.js
  * @Last modified by:   edl
- * @Last modified time: 15:38:16, 17-Jun-2019
+ * @Last modified time: 14:32:45, 12-Jul-2019
  */
 
 
